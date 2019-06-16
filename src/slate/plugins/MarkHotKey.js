9f8b8b681fd676fb66c0a70d83da15a3b0092fb0
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-06-16 10:18:24 -0300

    Ctrl -> CMD for macs

--- src/slate/plugins/MarkHotKey.js
+++ src/slate/plugins/MarkHotKey.js
@@ -5,7 +5,14 @@ const _onKeyDown = options => (event, editor, next) => {
   editor.toggleMark(options.type);
 }
 
-const _isTargetKey = (event, key) => (event.ctrlKey && event.key === key);
+const _isTargetKey = (event, key) => {
+  const mod = _getPlatformCommandKey(event);
+  return mod && event.key === key;
+};
+
+const _isMacOS = () => /Mac/.test(navigator.platform);
+
+const _getPlatformCommandKey = (event) => _isMacOS ? event.metaKey : event.ctrlKey;
 
 function MarkHotKey(options){
   return { onKeyDown: _onKeyDown(options) };
