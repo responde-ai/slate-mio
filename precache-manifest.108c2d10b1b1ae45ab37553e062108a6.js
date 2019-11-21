6eae73cf18cc382440bb32cc5b78b50d542a4383
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2019-11-21 00:12:08 -0300

    Updates

---
+++ precache-manifest.108c2d10b1b1ae45ab37553e062108a6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486bb5665034265884236f124fb6d569",
+    "url": "/slate-mio/index.html"
+  },
+  {
+    "revision": "ca42b9f8ee57a83e83a5",
+    "url": "/slate-mio/static/css/main.120d6f03.chunk.css"
+  },
+  {
+    "revision": "9861b1a12256878286cb",
+    "url": "/slate-mio/static/js/2.a3ae4a0a.chunk.js"
+  },
+  {
+    "revision": "ca42b9f8ee57a83e83a5",
+    "url": "/slate-mio/static/js/main.4c7f0dc9.chunk.js"
+  },
+  {
+    "revision": "3d949acb85dd239c06b0",
+    "url": "/slate-mio/static/js/runtime~main.47aa8b2a.js"
+  },
+  {
+    "revision": "2c0862a67bfb8773d52c7833e17cd5ca",
+    "url": "/slate-mio/static/media/arrow-down-icon.2c0862a6.svg"
+  },
+  {
+    "revision": "f50b201a666c00d04a354355bdc9af4c",
+    "url": "/slate-mio/static/media/bold-icon.f50b201a.svg"
+  },
+  {
+    "revision": "63a3df1d1a890f3b26f6676b8f03c31d",
+    "url": "/slate-mio/static/media/c-icon.63a3df1d.svg"
+  },
+  {
+    "revision": "8c6b92e1a7569933d32fb436ffc610a6",
+    "url": "/slate-mio/static/media/close-icon.8c6b92e1.svg"
+  },
+  {
+    "revision": "cf5e4039d88b7054bd1ac282e861a537",
+    "url": "/slate-mio/static/media/file-icon.cf5e4039.svg"
+  },
+  {
+    "revision": "205a843273edb243c80cffafdc778fcb",
+    "url": "/slate-mio/static/media/image-icon.205a8432.svg"
+  },
+  {
+    "revision": "7fd46e7260122158bbcc4dfb517012b5",
+    "url": "/slate-mio/static/media/italic-icon.7fd46e72.svg"
+  },
+  {
+    "revision": "8db9cd685111f6d953eb15bc5a3685fb",
+    "url": "/slate-mio/static/media/js-icon.8db9cd68.svg"
+  },
+  {
+    "revision": "a9e79322b4ba4d52cff125e361092cbb",
+    "url": "/slate-mio/static/media/list-ol-icon.a9e79322.svg"
+  },
+  {
+    "revision": "97954a53c7f59975b13f1cd1002dccc4",
+    "url": "/slate-mio/static/media/list-ul-icon.97954a53.svg"
+  },
+  {
+    "revision": "bd2da21d188d1d0a3d3ea6d90cf8d1e2",
+    "url": "/slate-mio/static/media/loading-icon.bd2da21d.svg"
+  },
+  {
+    "revision": "36f03ff63bc3d6e0c5505ff120d9ca5f",
+    "url": "/slate-mio/static/media/math-icon.36f03ff6.svg"
+  },
+  {
+    "revision": "7a4fa2fbd1eebe93bee50f5082c50ffb",
+    "url": "/slate-mio/static/media/python-icon.7a4fa2fb.svg"
+  },
+  {
+    "revision": "23e2c5a898d598c5a3cd28cd92a3d8a4",
+    "url": "/slate-mio/static/media/ruby-icon.23e2c5a8.svg"
+  },
+  {
+    "revision": "64bb4a16160cc700cd37d14a546377de",
+    "url": "/slate-mio/static/media/strikethrough-icon.64bb4a16.svg"
+  },
+  {
+    "revision": "48aa0e98263a0ec043610650611a9a41",
+    "url": "/slate-mio/static/media/submit-icon.48aa0e98.svg"
+  },
+  {
+    "revision": "c87393cbce209cadf192b0c84ef2d3e7",
+    "url": "/slate-mio/static/media/title-icon.c87393cb.svg"
+  },
+  {
+    "revision": "cc647f2fa8725956c8d51b6caac51137",
+    "url": "/slate-mio/static/media/underline-icon.cc647f2f.svg"
+  }
+]);
